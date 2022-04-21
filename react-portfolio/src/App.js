03d9a2a5ef55028e5f2c8b4577498199fc189e41
Jotas <jproodian721@gmail.com>
2022-04-21 12:25:41 -0500

    setting up folder structure

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import About from './components/AboutMe';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+
+function App() {
+  return (
+
+    // useState for which component is being rendered
+
+asdf
+    
+  );
+}
+
+export default App;
